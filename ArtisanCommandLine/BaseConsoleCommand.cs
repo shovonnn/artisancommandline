5815d6ba411612a6c07c5586a319d18789f346f8
Mahfuz Khan <mahfuzkhanshovon@gmail.com>
2017-12-23 22:09:51 +0600

    added base command class with service inject capability

---
+++ ArtisanCommandLine/BaseConsoleCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+namespace ArtisanCommandLine
+{
+    public abstract class BaseConsoleCommand
+    {
+        private IServiceProvider serviceProvider;
+
+        public virtual void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Initialize()
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        public object ResolveType(Type type)
+        {
+            return serviceProvider.GetService(type);
+        }
+    }
+}
